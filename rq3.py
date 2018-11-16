3b1066f663b651b562c5a6765b0de1caf7492e95
Luyao Ren <375833274@qq.com>
2018-11-16 05:13:55 -0500

    update rq2

--- rq3.py
+++ rq3.py
@@ -20,7 +20,16 @@ if os.path.exists(file):
 # print(last_run)
 print('last_run', len(last_run))
 
-from detect import *
+import detect
+
+detect.speed_up = False
+detect.filter_larger_number = True
+detect.filter_out_too_old_pull_flag = True
+detect.filter_already_cite = False
+detect.filter_create_after_merge = False
+detect.filter_overlap_author = False
+detect.filter_out_too_big_pull_flag = False
+detect.filter_same_author_and_already_mentioned = True
 
 out = open(file, 'a+')
 
@@ -43,27 +52,13 @@ with open(input_pairs) as f:
         if (r, n1, n2) == ('cocos2d/cocos2d-x', '11441', '11583'):
             continue
 
-
         if (r, n1, n2) in last_run:
             li = last_run[(r, n1, n2)]
         else:
-            li = [int(x[0]) for x in get_topK(r, n2, 30, True, way)]
-            
-            print(r, n1, n2, 'ret =', li[0])
+            li = [int(x[0]) for x in detect.get_topK(r, n2, 30, True, way)]            
+            print(r, n1, n2, 'ret =', li)
             print(r, n1, n2, li, file=out)
             out.flush()
 
-        cnt_num += 1
-        for i in range(30):
-            if int(n1) in li[:i+1]:
-                top_acc[i] += 1
-        print('now=', r, cnt_num, 'top1 acc =', 1.0 * top_acc[0] / cnt_num)
-        
-
 print('end!')
-for i in range(30):
-    t = 1.0 * top_acc[i] / cnt_num
-    top_acc[i] = t
-    print('top%d acc =%.4f' % (i+1, t))
-
 out.close()
